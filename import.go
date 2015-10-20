7668e534d4a1eb5ef85f2fe4184adc3809e298d2
Aleksey Filippov <sarum9in@gmail.com>
2015-10-20 19:58:29 +0400

    Fixed Go style.

--- import.go
+++ import.go
@@ -1,31 +1,31 @@
 package buildutils
 
 import (
-    "fmt"
-    "go/build"
-    "path/filepath"
-    "strings"
+	"fmt"
+	"go/build"
+	"path/filepath"
+	"strings"
 )
 
 func SrcDir(importPath string) (string, error) {
-    pkg, err := build.Import(importPath, "", build.FindOnly)
-    if err != nil {
-        return "", err
-    }
-    return pkg.Dir, nil
+	pkg, err := build.Import(importPath, "", build.FindOnly)
+	if err != nil {
+		return "", err
+	}
+	return pkg.Dir, nil
 }
 
 func importPath(srcDir string, srcDirs []string) (string, error) {
-    for _, srcPath := range srcDirs {
-        if rel, err := filepath.Rel(srcPath, srcDir); err == nil &&
-            !strings.HasPrefix(rel, "..") {
+	for _, srcPath := range srcDirs {
+		if rel, err := filepath.Rel(srcPath, srcDir); err == nil &&
+			!strings.HasPrefix(rel, "..") {
 
-            return rel, nil
-        }
-    }
-    return "", fmt.Errorf("unable to find import path for source %q", srcDir)
+			return rel, nil
+		}
+	}
+	return "", fmt.Errorf("unable to find import path for source %q", srcDir)
 }
 
 func ImportPath(srcDir string) (string, error) {
-    return importPath(srcDir, build.Default.SrcDirs())
+	return importPath(srcDir, build.Default.SrcDirs())
 }
--- import_test.go
+++ import_test.go
@@ -1,74 +1,74 @@
 package buildutils
 
 import (
-    "os"
-    "path"
-    "testing"
+	"os"
+	"path"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
-    CurrentImportPath = "github.com/bunsanorg/buildutils"
-    CurrentFileName   = "import_test.go"
+	CurrentImportPath = "github.com/bunsanorg/buildutils"
+	CurrentFileName   = "import_test.go"
 )
 
 func TestSrcDir(t *testing.T) {
-    dir, err := SrcDir(CurrentImportPath)
-    require.NoError(t, err)
-    _, err = os.Stat(path.Join(dir, CurrentFileName))
-    assert.NoError(t, err, "unable to find current file")
+	dir, err := SrcDir(CurrentImportPath)
+	require.NoError(t, err)
+	_, err = os.Stat(path.Join(dir, CurrentFileName))
+	assert.NoError(t, err, "unable to find current file")
 }
 
 func TestImportPath(t *testing.T) {
-    dir, err := SrcDir(CurrentImportPath)
-    require.NoError(t, err)
-    importPath, err := ImportPath(dir)
-    require.NoError(t, err)
-    assert.Equal(t, CurrentImportPath, importPath)
+	dir, err := SrcDir(CurrentImportPath)
+	require.NoError(t, err)
+	importPath, err := ImportPath(dir)
+	require.NoError(t, err)
+	assert.Equal(t, CurrentImportPath, importPath)
 }
 
 func TestImportPathRaw(t *testing.T) {
-    testData := []struct {
-        srcDir     string
-        srcDirs    []string
-        importPath string
-        errString  string
-    }{
-        {
-            srcDir:     "/root/1/import/path",
-            srcDirs:    []string{"/root/1", "/root/2", "/root/3"},
-            importPath: "import/path",
-            errString:  "",
-        },
-        {
-            srcDir:     "/root/2/import/path",
-            srcDirs:    []string{"/root/1", "/root/2", "/root/3"},
-            importPath: "import/path",
-            errString:  "",
-        },
-        {
-            srcDir:     "/root/3/import/path",
-            srcDirs:    []string{"/root/1", "/root/2", "/root/3"},
-            importPath: "import/path",
-            errString:  "",
-        },
-        {
-            srcDir:     "/root/4/import/path",
-            srcDirs:    []string{"/root/1", "/root/2", "/root/3"},
-            importPath: "",
-            errString:  "unable to find import path for source \"/root/4/import/path\"",
-        },
-    }
-    for _, tt := range testData {
-        impPath, err := importPath(tt.srcDir, tt.srcDirs)
-        if tt.errString == "" {
-            if assert.NoError(t, err) {
-                assert.Equal(t, tt.importPath, impPath)
-            }
-        } else {
-            assert.EqualError(t, err, tt.errString)
-        }
-    }
+	testData := []struct {
+		srcDir     string
+		srcDirs    []string
+		importPath string
+		errString  string
+	}{
+		{
+			srcDir:     "/root/1/import/path",
+			srcDirs:    []string{"/root/1", "/root/2", "/root/3"},
+			importPath: "import/path",
+			errString:  "",
+		},
+		{
+			srcDir:     "/root/2/import/path",
+			srcDirs:    []string{"/root/1", "/root/2", "/root/3"},
+			importPath: "import/path",
+			errString:  "",
+		},
+		{
+			srcDir:     "/root/3/import/path",
+			srcDirs:    []string{"/root/1", "/root/2", "/root/3"},
+			importPath: "import/path",
+			errString:  "",
+		},
+		{
+			srcDir:     "/root/4/import/path",
+			srcDirs:    []string{"/root/1", "/root/2", "/root/3"},
+			importPath: "",
+			errString:  "unable to find import path for source \"/root/4/import/path\"",
+		},
+	}
+	for _, tt := range testData {
+		impPath, err := importPath(tt.srcDir, tt.srcDirs)
+		if tt.errString == "" {
+			if assert.NoError(t, err) {
+				assert.Equal(t, tt.importPath, impPath)
+			}
+		} else {
+			assert.EqualError(t, err, tt.errString)
+		}
+	}
 }
